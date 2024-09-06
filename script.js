c09710c37fe68af3a08314947ad9cad636ca8067
Zak Le Bentz <zakerylebentz@gmail.com>
2024-09-06 15:16:04 +0100

    First push, initial skeleton set-up

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("testline1");
+
+let images = [
+  {
+    src: "https://unsplash.com/photos/aerial-photography-of-gray-concrete-road-between-assorted-color-trees-Qy-CBKUg_X8",
+    alt: "a road inbetween a forest whcih trees range from green to orange leacves",
+    width: "300",
+    height: "200",
+  },
+  {
+    src: "https://unsplash.com/photos/a-road-with-trees-on-either-side-kLI3J6nlFzM",
+    alt: "autumnal picture of a road through a forest",
+    width: "300",
+    height: "200",
+  },
+];
+
+function createThumbnails(thumbnailContainer) {
+  thumbnailContainer.forEach(function (thumbnail, index) {});
+}
+
+function createLargeImage() {}
